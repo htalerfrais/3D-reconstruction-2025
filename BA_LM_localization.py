13a7bb1a03c28d0e96f1a25cba48f6ee21f59f21
mhurtubise-git <maxime.hurtubise@gmx.fr>
2026-01-06 17:37:33 +0100

    First Commit

--- BA_LM_localization.py
+++ BA_LM_localization.py
@@ -1,10 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 16 11:19:59 2025
+import numpy as np
+from BA_LM_schur import BA_LM_schur
 
-@author: hector
-"""
+def BA_LM_localization(Mwc_init, Xw, x2d, K, maxIt=20):
 
-def BA_LM_localization():
-    return 0
+    tracks = [{
+        'p3D_keys': np.arange(Xw.shape[0]),
+        'p2D_ids': np.arange(x2d.shape[0])
+    }]
 
+    p = [x2d]
+    p3D_keys_to_ids = np.arange(Xw.shape[0])
+    Mwc_init_list = [Mwc_init]
+
+    BA = BA_LM_schur(
+        Mwc_init=Mwc_init_list,
+        Uw_init=Xw.copy(),
+        p3D_keys_to_ids=p3D_keys_to_ids,
+        tracks=tracks,
+        K=K,
+        p=p,
+        maxIt=maxIt
+    )
+    
+    Uw_fixed = BA.Uw.copy()
+    BA.optimize()
+    BA.Uw[:] = Uw_fixed
+
+    return BA.getPoses()[0]
--- main_reconstruction.py
+++ main_reconstruction.py
@@ -30,7 +30,7 @@ with open('data_ready.pkl', 'rb') as f:
 
 imA_id = 0
 imB_id = 1
-assert(imA_id== 0 and imB_id==1)
+assert(imA_id==0 and imB_id==1)
 
 _, idsA, idsB = np.intersect1d(tracks_full[imA_id]['p3D_keys'], tracks_full[imB_id]['p3D_keys'], assume_unique=False, return_indices=True)
 
@@ -59,32 +59,30 @@ p_B = p[imB_id][tracks_full[imB_id]['p2D_ids'][idsB],:]
 p_B_hom = np.ones((p_B.shape[0],3))
 p_B_hom[:,:2] = p_B.copy()
 
-#%% INITIALISATION - Get relative pose from essential (obtained using RANSAC+five-point algorithm)
+#%% INITIALISATION - Get relative pose from essential (obtained using RANSAC + five-point algorithm)
 
 K_inv = np.linalg.inv(K)
-m_B_hom = p_B_hom @ K_inv.T # passage dans le plan focal pour find essential
+m_B_hom = p_B_hom @ K_inv.T
 m_A_hom = p_A_hom @ K_inv.T
 E_AB, _ = cv.findEssentialMat(m_B_hom[:,:2],m_A_hom[:,:2],method=cv.LMEDS)
                    
 nInliersChirality, R_BA, t_BA, maskChirality_cv = cv.recoverPose(E_AB.T, p_A_hom[:,:2], p_B_hom[:,:2], K)
-maskChirality = (maskChirality_cv!=0).flatten() # maskChirality : info sur points devant caméra
+maskChirality = (maskChirality_cv!=0).flatten()
 t_BA = t_BA.flatten()
 
-M_BA = np.eye(4) # rassemble t_BA et R_BA dans une meme matrice de Projection qui va du repere B a A
+M_BA = np.eye(4)
 M_BA[:3,:3] = R_BA
 M_BA[:3,3] = t_BA
 
 p_A_filter = p_A_hom[maskChirality,:]
 p_B_filter = p_B_hom[maskChirality,:]
 
-# comme repere A = repere monde (W) alors M_BA est matrice qui passe de B a repère Monde aussi 
-P_AW = K @ np.hstack((np.eye(3), np.zeros((3,1)))) # matrice extrinseque alignee avec repere mondiale
-P_BW = K @ M_BA[:3,:] 
+P_AW = K @ np.hstack((np.eye(3), np.zeros((3,1))))
+P_BW = K @ M_BA[:3,:]
 U_A_hom = cv.triangulatePoints(P_AW, P_BW, p_A_filter[:,:2].T, p_B_filter[:,:2].T)
 U_A = (U_A_hom[:3,:]/U_A_hom[3,:]).T
 assert (U_A[:,2] > 0).all() #all 3D points should have a positive depth in A
 
-# tentative de reprojection basee sur les points 3D obtenus avec la matrice essentielle & triangulation
 p_A_pred = (U_A/U_A[:,2:3]) @ K.T
 p_B_pred = ((U_B := (U_A @ R_BA.T) + t_BA)/U_B[:,2:3]) @ K.T
 
@@ -116,22 +114,14 @@ p3D_keys_reconstructed = tracks[imA_id]['p3D_keys']
 
 #%% INITIALISATION - Bundle Adjustment
 
-# calcul erreure de reprojection après avoir estimé les Rotations et translations puis triangulé
-# Pour calculer optimiser les paramètres (U, t, R) de cette erreure de reprojection on utilise 
-# moindre carrés linéaires. Pour résoudre cela on a besoin d'une bonne initialisation qui se fait 
-# d'abord avec l'estimation de la matrice Essentielle en geom epipolaire comme fait au dessus.
-# Puis on linéarise la le pb d'optimisation, puis on résoud avec Levenberg Marquardt
-
-
 I = [I_A, I_B]
-Mwc = [np.eye(4), np.linalg.inv(M_BA)] # matrices de passage caméra vers word (eye(4) est l'origine (caméra A))
-Uw = U_A.copy() # replacer les premiers points 3D issus 
+Mwc = [np.eye(4), np.linalg.inv(M_BA)]
+Uw = U_A.copy()
 
 Mwc, Uw = utils.normalizeReconstructionScale(Mwc,Uw)
 
 nCam = len(Mwc)
 
-# calcul des erreures de reprojection avant de commencer le Bundle Adjustment 
 fig4, axs4 = plt.subplots(ncols=nCam)
 fig4.suptitle('Initial pair : reproj error before BA')
 for cam in range(nCam):
@@ -172,7 +162,7 @@ Mwc = BA.getPoses()
 Uw = BA.getPointCloud()
 
 fig5, axs5 = plt.subplots(ncols=nCam)
-fig5.suptitle('Initial pair : reproj error before BA')
+fig5.suptitle('Initial pair : reproj error after BA')
 for cam in range(nCam):
     Mwc_cur = Mwc[cam]
     U_c = (Uw[p3D_keys_to_ids[tracks[cam]['p3D_keys']],:] - Mwc_cur[:3,3])@Mwc_cur[:3,:3]
@@ -196,53 +186,81 @@ plt.pause(0.1)
 fig6, axs6 = plt.subplots(ncols=2)
 fig6.suptitle('Current image : reproj error localization')
 
-#%%
+ 
+for new_im_id in range(2, len(im_names)):        #len(im_names)
+    print('Image Index : ', new_im_id)
+    #% Add image
+     
+    #search keys of 3D points already reconstructed and seen in the new image -> np.intersect1d
+    common_keys, idsReconstruct, idsNew = np.intersect1d(p3D_keys_reconstructed, tracks_full[new_im_id]['p3D_keys'], assume_unique=False, return_indices=True)
 
-for new_im_id in range(2, len(im_names)):
-    #%% Add image (initialize for a new image)
+    #remove 3D points seen in the new image that have a negative depth before localisation
+    Mwc_guess = Mwc[-1]         #approx new camera position by previous one
+    Rwc_guess = Mwc_guess[:3, :3]
+    twc_guess = Mwc_guess[:3, 3]
     
-    pass 
-    # search keys of 3D points already reconstructed and seen in the new image -> np.intersect1d
-    _, idsReconstruct, idsNew = np.intersect1d(p3D_keys_reconstructed, tracks_full[new_im_id]['p3D_keys'], assume_unique=False, return_indices=True)
+    Xw = Uw[p3D_keys_to_ids[common_keys]]           #3D points in world
+    x2d = p[new_im_id][tracks_full[new_im_id]['p2D_ids'][idsNew], :]        #corresponding 2D points
+    U_c = (Xw - twc_guess) @ Rwc_guess          #project to camera 
     
-    # remove 3D points seen in the new image that have a negative depth before localisation
-    # cette étape peut être ignorée pour l'instant et reconstruire les points 3D même si ils se trouvent être à l'arriere de la caméra
+    mask = U_c[:, 2] > 1e-6       #mask
+    U_c = U_c[mask]             #apply mask
+    Xw = Xw[mask]
+    p_c_loc = x2d[mask]
     
-    # all 3D points seen in the new image should have positive depth ->  assert(np.all(U_c[:,2] > 0.)) 
+    p_c_loc_pred = (U_c / U_c[:, 2:3]) @ K.T    #get the projected 2D points of the 3D points into Mwc_guess
+    p_c_loc_pred = p_c_loc_pred[:, :2]
     
+    #all 3D points seen in the new image should have positive depth ->  assert(np.all(U_c[:,2] > 0.)) 
+    assert(np.all(U_c[:,2] > 0.))
     
     I_c= np.array(Image.open(join('images',im_names[new_im_id]))) #load new image for visualization and debug
     I.append(I_c) # store new image for visualization and debug
     
+    reproj_error = (1/p_c_loc_pred.shape[0])*np.sqrt(((p_c_loc_pred[:,:2] - p_c_loc)**2).sum(axis=1)).sum()
+    
     #Visualize reprojection error before localization
-    
-    # initialiser les paramètres pour la nouvelle image et en déduire une première reprojection 
-    # comme cela avait été fait dans la première partie pour la première caméra.
-    
-    
-    # axs6[0].clear()
-    # plt.pause(0.1)
-    # axs6[0].imshow(I[new_im_id])
-    # axs6[0].scatter(p_c_loc[:,0], p_c_loc[:,1], marker ='o', facecolors='none', edgecolors='r')
-    # axs6[0].scatter(p_c_loc_pred[:,0], p_c_loc_pred[:,1], marker ='x', color='b')
-    # axs6[0].plot(np.vstack((p_c_loc[:,0],p_c_loc_pred[:,0])), np.vstack((p_c_loc[:,1],p_c_loc_pred[:,1])))
-    # axs6[0].set_title('Loc. Init. Reproj. err. {0:0.2f} pix'.format(reproj_error))
-    # plt.pause(0.1)
+    axs6[0].clear()
+    plt.pause(0.1)
+    axs6[0].imshow(I[new_im_id])
+    axs6[0].scatter(p_c_loc[:,0], p_c_loc[:,1], marker ='o', facecolors='none', edgecolors='r')
+    axs6[0].scatter(p_c_loc_pred[:,0], p_c_loc_pred[:,1], marker ='x', color='b')
+    axs6[0].plot(np.vstack((p_c_loc[:,0],p_c_loc_pred[:,0])), np.vstack((p_c_loc[:,1],p_c_loc_pred[:,1])))
+    axs6[0].set_title('Loc. Init. Reproj. err. {0:0.2f} pix'.format(reproj_error))
+    plt.pause(0.25)
     
     #%% LOCALISATION
+    Mwc_new = BA_LM_localization(
+        Mwc_guess,
+        Xw,
+        x2d,
+        K
+    )
     
+    Mwc.append(Mwc_new)
     
+    Rwc = Mwc_new[:3, :3]
+    twc = Mwc_new[:3, 3]
     
-    pass 
+    U_c = (Xw - twc) @ Rwc
+    p_c_loc_pred = (U_c / U_c[:, 2:3]) @ K.T
+    p_c_loc_pred = p_c_loc_pred[:, :2]
+    
+    reproj_error = np.mean(
+        np.linalg.norm(p_c_loc_pred - p_c_loc, axis=1)
+    )
+    
+    print(f"Localization reprojection error: {reproj_error:.2f} px")
+     
     #Visualize reprojection error after localization
-    # axs6[1].clear()
-    # plt.pause(0.1)
-    # axs6[1].imshow(I[new_im_id])
-    # axs6[1].scatter(p_c_loc[:,0], p_c_loc[:,1], marker ='o', facecolors='none', edgecolors='r')
-    # axs6[1].scatter(p_c_loc_pred[:,0], p_c_loc_pred[:,1], marker ='x', color='b')
-    # axs6[1].plot(np.vstack((p_c_loc[:,0],p_c_loc_pred[:,0])), np.vstack((p_c_loc[:,1],p_c_loc_pred[:,1])))
-    # axs6[1].set_title('Loc. Refined. Reproj. err. {0:0.2f} pix'.format(reproj_error))
-    # plt.pause(0.1)
+    axs6[1].clear()
+    plt.pause(0.1)
+    axs6[1].imshow(I[new_im_id])
+    axs6[1].scatter(p_c_loc[:,0], p_c_loc[:,1], marker ='o', facecolors='none', edgecolors='r')
+    axs6[1].scatter(p_c_loc_pred[:,0], p_c_loc_pred[:,1], marker ='x', color='b')
+    axs6[1].plot(np.vstack((p_c_loc[:,0],p_c_loc_pred[:,0])), np.vstack((p_c_loc[:,1],p_c_loc_pred[:,1])))
+    axs6[1].set_title('Loc. Refined. Reproj. err. {0:0.2f} pix'.format(reproj_error))
+    plt.pause(0.25)
     
 
     #%% TRIANGULATION of new 3D points
